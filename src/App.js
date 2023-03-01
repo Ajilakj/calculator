70da2af86cca2c0058c5e19bdaa974726613a483
Ajila K Johnkutty <kjajila@gmail.com>
2023-03-01 13:18:18 -0500

    add UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [result, setResult] = useState('');
+
+  const handleClick = () =>{
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form>
+        <input className="current-operand" type={"text"} value={result}/>
+      </form>
+      <div clasName="buttons">
+        <button clasName="ac span-two" onClick={handleClick}>AC</button>
+        <button onClick={handleClick}>Del</button>
+        <button onClick={handleClick}>&divide;</button>
+        <button onClick={handleClick}>7</button>
+        <button onClick={handleClick}>8</button>
+        <button onClick={handleClick}>9</button>
+        <button onClick={handleClick}>&times;</button>
+        <button onClick={handleClick}>4</button>
+        <button onClick={handleClick}>5</button>
+        <button onClick={handleClick}>6</button>
+        <button onClick={handleClick}>-</button>
+        <button onClick={handleClick}>1</button>
+        <button onClick={handleClick}>2</button>
+        <button onClick={handleClick}>3</button>
+        <button onClick={handleClick}>+</button>
+        <button onClick={handleClick}>0</button>
+        <button onClick={handleClick}>.</button>
+        <button clasName="span-two" onClick={handleClick}>=</button>
+      </div>
     </div>
   );
 }
